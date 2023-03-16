5750b6b93579431ae6bbdf935cf6f6fded6ed9a0
abdelrahmanShabaan <boodyelmasry6@gmail.com>
2023-03-16 18:52:51 +0200

    finish with delete items

--- src/components/todoItems/todoitems.js
+++ src/components/todoItems/todoitems.js
@@ -3,15 +3,20 @@ import './Todoitems.css'
 const TodoItems = (props) => {
 
     const {items , deleteItem} = props;
-    const ListItems = items.map(item => {
-        return(
-            <div key={item.id}>
-                <span>{item.name}</span>
-                <span>{item.age}</span>
-                <span onClick={ ()=> deleteItem(item.id)}>&times;</span>
-            </div>
-        )
-    })
+    let length = items.length; 
+    const ListItems = length ? (
+        items.map(item => {
+            return(
+                <div key={item.id}>
+                    <span>{item.name}</span>
+                    <span>{item.age}</span>
+                    <span onClick={ ()=> deleteItem(item.id)}>&times;</span>
+                </div>
+            )
+        })
+    ) : (
+        <p>There is no items</p>
+    )
     return (
         <div className="App">
             <div>
