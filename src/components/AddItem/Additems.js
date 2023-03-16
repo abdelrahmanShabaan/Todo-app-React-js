4690f7c0d8039b84479041098559f01a6a482946
abdelrahmanShabaan <boodyelmasry6@gmail.com>
2023-03-16 18:03:33 +0200

    added import for css in components

--- src/components/AddItem/Additems.js
+++ src/components/AddItem/Additems.js
@@ -1,5 +1,5 @@
 import React ,{Component} from "react";
-
+import './Additems.css'
 
 class AddItem extends Component{
     render() {
--- src/components/todoItems/todoitems.js
+++ src/components/todoItems/todoitems.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import './Todoitems.css'
 const TodoItems = () =>{
     return (
         <div>
